2321a450973862acb288acf41c9d5c513310d8b6
Luiz Marques <luizcardineli@gmail.com>
2025-07-19 05:40:25 -0300

    Create Exerc02.sql

---
+++ Aulas/Exerc02.sql
@@ -0,0 +1,7 @@
+-- x1. Faça uma query que apresente o tamanho médio, máximo e mínimo da descrição do objeto por categoria
+
+-- Ex2. Faça uma query que apresente o tamanho médio, máximo e mínimo do nome do objeto por categoria
+
+-- Ex3. Faça uma query que apresente o tamanho médio, máximo e mínimo do nome do objeto por categoria. Considere apenas os objetos que tenham a descrição maior que 100.
+
+-- Ex4. Faça uma query que apresente o tamanho médio, máximo e mínimo do nome do objeto por categoria. Considere apenas os objetos que tenham a descrição maior que 100. Exiba apenas as categorias com tamanho médio de descrição do objeto maior que 500 caracteres.
